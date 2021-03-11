8a4655ab31d34c46e031741d1dc2f95aa12890e6
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-11 22:23:57 +0100

    cw15 pobieranie danych z klawiatury i przypisanie ich

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -3,21 +3,26 @@ package com.company;
 import com.company.vehicle.Car;
 import com.company.vehicle.Engine;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        number11(15);
-    }
-    public static void number11(int a){
-        for (int j = 0; j < a; j++)  {
-            if (j % 2 == 0){
-                continue;
-            }
-            if (j % 11 == 0) {
-                break;
-            }
-            System.out.println(j);
-        }
+        System.out.println("nazwa samochodu: ");
+        String name = scanner.nextLine();
+        System.out.println("mocs silnika: ");
+        Integer power = scanner.nextInt();
+
+        Engine engine = new Engine();
+        engine.setPower(power);
+        Car car = new Car();
+        car.setName(name);
+        car.setEngine(new Engine());
+        car.setEngine(engine);
+
+        System.out.println("nazwa samochodu to: " + car.getName());
+        System.out.println("mocs silnika to: " + car.getEngine().getPower());
     }
 }
