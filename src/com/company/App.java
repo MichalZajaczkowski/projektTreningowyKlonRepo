c5239f399d236a2444d3f9b2cc6b5de12a7c7a31
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-11 18:06:17 +0100

    reconstruction calculator

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -6,43 +6,8 @@ import com.company.vehicle.Engine;
 public class App {
 
     public static void main(String[] args) {
-        //Car.staticMethod();metody statyczne mogę uruchamiać
-        // bezpośrednio na klasie nie potrzebuję od tego obiektu
 
-        Engine engine1 = new Engine();
-        engine1.setPower(150);
-        engine1.setCapacity(1500);
-        Engine engine2= new Engine();
-        engine2.setPower(250);
-        engine2.setCapacity(2500);
-
-        Car Car1 = new Car("Car1Name");
-        Car1.setEngine(engine1);
-        Car Car2 = new Car("Car2Name");
-        Car2.setEngine(engine2);
-
-        Car1.printName();
-        Car1.getEngine().printPower();
-        Car1.getEngine().printCapacity();
-
-        Car2.printName();
-        Car2.getEngine().printPower();
-        Car2.getEngine().printCapacity();
-
-        //Car2.engine => engine2
-        //engine2.power => Car2.engine.power
-
-        Calculator calculator = new Calculator(15,7);
-        Calculator calculator2 = new Calculator(1574,77);
-        Calculator calculator3 = new Calculator(2135,67);
-        Calculator calculator4 = new Calculator(4451,227);
-
-        calculator.printCalculator();
-        calculator2.printCalculator();
-        calculator3.printCalculator();
-        calculator4.printCalculator();
-
-
-
+        Calculator calculator = new Calculator();
+        
     }
 }
--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,15 +1,8 @@
 package com.company;
 
 public class Calculator {
-    private Integer a;
-    private Integer b;
-
-    public Calculator (Integer a, Integer b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public void printCalculator(){ // metoda bezparametrowa
-        System.out.println(a.doubleValue() / b);
+    public Double calculator(Integer a, Integer b){
+       Double result =  a.doubleValue() / b;
+       return result;
     }
 }
