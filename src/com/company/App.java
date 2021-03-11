87e10f32c75c489c8bc8a3c8335dc602bd52c9d8
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-11 21:06:04 +0100

    cw13 pętla z prezrwaniem

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -7,24 +7,17 @@ public class App {
 
     public static void main(String[] args) {
 
-        Integer i = 5;
-        while (i > 0) {
-            System.out.println(i);
-            i--;
-        }
-        System.out.println("_");
-        Integer q = 5;
-        do {
-            q--;
-            System.out.println(q);
-        } while (q > 0);
-        System.out.println("_");
-
-        Integer value = 10;
-        for (int j = 0; j < value; j++)  {
+        number11(15);
+    }
+    public static void number11(int a){
+        for (int j = 0; j < a; j++)  {
+            if (j % 2 == 0){
+                continue;
+            }
+            if (j % 11 == 0) {
+                break;
+            }
             System.out.println(j);
-
         }
-
     }
 }
