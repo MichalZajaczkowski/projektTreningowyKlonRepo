9250b3e83b68113cd237dec154709ff3825d8047
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-13 19:07:05 +0100

    cw20-pobranie tekstu, wywietlenie info o ilosci wyrazow i wypisanie ich

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -1,26 +1,26 @@
 package com.company;
 
-import com.company.vehicle.Car;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
- /*       Color color = Color.RED;
-        System.out.println(color);
-        System.out.println(color.ordinal());
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        s = s.trim();
+        System.out.println(wordCounter(s));
+    }
 
-        Color[] values = Color.values();
-        for (int i = 0; i < values.length; i++) {
-            System.out.println(values[i]);
+    private static int wordCounter(String s) {
+        String[] strings = s.split(" ");
+        int counter = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].isEmpty()){
+               continue;
+            }
+            counter++;
+            System.out.println("." + strings[i] + ".");
         }
-
-        Color color1 = Color.valueOf("A");
-        System.out.println(color1);
-        System.out.println(color1.ordinal());*/
-
-        Car car = new Car();
-        car.setColor(Color.RED);
-        System.out.println(car.getColor());
-        System.out.println(car.getColor().getCarColorPl());
+        return counter;
     }
 }
