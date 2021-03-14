c6c2c88d2027361ab6529f569638cee608ad3dc6
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-14 14:56:20 +0100

    tworzenie i zczytywanie pliku

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -1,21 +1,23 @@
 package com.company;
 
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        printNames("a", "b");
-        printNames("A", "B", "C", "D");
-    }
-    private static void printNames(String...strings){
- /*       for (int i = 0; i < strings.length; i++) {
-            System.out.println(strings[i]);
-        }*/
+        Path path = Paths.get("src","com", "company", "file.txt");
 
-        for (String s: strings) {
-            System.out.println(s);
+        try {
+            //Files.createFile(path);
+            List<String> strings = Files.readAllLines(path);
+            for (String s: strings) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
