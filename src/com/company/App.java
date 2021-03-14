66fd7eb943ff74d9bfcf8036e1f249be2b1b3eb6
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-14 13:06:07 +0100

    cw22 pobranie wartosci z scanera, obsluga wyjatku jesli pobierzesz bledna wartosc

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -6,12 +6,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-        try{
-            double ourNumber = scanner.nextDouble();
-            System.out.println(ourNumber);
-        } catch (InputMismatchException e){
-            System.out.println("musisz podac liczbę");
-        }
+
+         boolean flag = true;
+         while (flag){
+             try{
+                 Scanner scanner = new Scanner(System.in);
+                 System.out.print("podaj liczbę całkowitą: ");
+                 int ourNumber = scanner.nextInt();
+                 System.out.println("podano: " + ourNumber);
+                 flag = false;
+                 //break;
+             } catch (InputMismatchException e){
+                 System.out.println("musisz podac poprawną wartość");
+             }
+         }
     }
 }
